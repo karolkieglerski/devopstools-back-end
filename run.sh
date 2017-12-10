ce23ebba270c645848127be7800a980c960d5757
Karol Kieglerski <kieglerski.karol@gmail.com>
2017-12-10 18:44:23 +0100

    add CORS to allow front-end connections

--- run.sh
+++ run.sh
@@ -6,5 +6,8 @@ export DATABASE_TEST_URL=postgres://postgres:postgres@localhost:5432/users_test
 
 python manage.py recreate_db
 python manage.py seed_db
-python manage.py test
-python manage.py cov
+if [[ $1 == 'test' ]]; then
+    python manage.py test
+    python manage.py cov
+fi
+python manage.py runserver -p 5555
